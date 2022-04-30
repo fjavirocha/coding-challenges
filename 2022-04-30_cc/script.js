389d4bfbd8511ae7fdaee5227eec0bbe8a20c87f
Javi Rocha <fjavierocha@gmail.com>
2022-04-30 10:46:28 -0700

    codewars - Abbreviate a Two Word Name

---
+++ 2022-04-30_cc/script.js
@@ -0,0 +1,17 @@
+//Abbreviate a Two Word Name
+
+// Write a function to convert a name into initials. This kata strictly takes two words with one space in between them.
+
+// The output should be two capital letters with a dot separating them.
+
+// It should look like this:
+
+// Sam Harris => S.H
+
+// patrick feeney => P.F
+
+function abbrevName(name){
+  return name.split(' ').map(item => item[0]).join('.').toUpperCase();
+}
+
+console.log(abbrevName('javi rocha'));
