56219fa3979b3cbfbc15845498e2afd6f2e8db6b
Javi Rocha <fjavierocha@gmail.com>
2022-05-01 20:04:39 -0700

    delete js file

--- codewars/8kyu/2022-04-23/script.js
+++
@@ -1,9 +0,0 @@
-// Removing String Spaces
-
-// Simple, remove the spaces from the string, then return the resultant string.
-
-function noSpace(x){
-  return x.split(' ').join('');
-}
-
-console.log(noSpace('javi Rocha Is Coding'))
