188ce89642f78bcf671932d4148cf289674b5693
Javi Rocha <fjavierocha@gmail.com>
2022-05-05 19:22:03 -0700

    codewars - descending order

---
+++ codewars/7kyu/2022-05-05/script.js
@@ -0,0 +1,9 @@
+// Descending Order
+
+// Your task is to make a function that can take any non-negative integer as an argument and return it with its digits in descending order. Essentially, rearrange the digits to create the highest possible number.
+
+function descendingOrder(n){
+  return n.toString().split('').sort((a, b) => b - a).join('') * 1;
+}
+
+console.log(descendingOrder(42145));
